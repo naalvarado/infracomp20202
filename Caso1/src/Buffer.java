caf116dff6452686a79137775002ed8766101b5f
na.alvarado <na.alvarado@uniandes.edu.co>
2020-09-01 11:25:05 -0500

    clase Mensaje y Buffer

--- Caso1/src/Buffer.java
+++ Caso1/src/Buffer.java
@@ -1,4 +1,55 @@
+import java.util.ArrayList;
 
 public class Buffer {
+	
+	private ArrayList<Mensaje> mensages;
+	private int max;
+	private int noMensajes;
+	
+	public Buffer(int maximo) {
+		this.max = maximo;
+		this.noMensajes = 0;
+		this.mensages = new ArrayList<Mensaje>();
+	}
+
+	public void addMensaje(Mensaje m) {
+		while(noMensajes == max) {
+			try {
+				wait();
+			}
+			catch(Exception e) {
+				// do non
+			}
+		}
+		synchronized(this) {
+			this.mensages.add(m);
+			noMensajes++;
+			notifyAll();
+		}
+	}
+	
+	public void removeMensaje(Mensaje m) {
+		while(noMensajes == 0) {
+			try {
+				wait();
+			}
+			catch(Exception e) {
+				//do non
+			}
+		}
+		synchronized(this) {
+			this.mensages.remove(m);
+			noMensajes--;
+			notifyAll();
+		}
+	}
+
+	public int getMax() {
+		return max;
+	}
+
+	public int getNoMensajes() {
+		return noMensajes;
+	}
 
 }
--- Caso1/src/Mensaje.java
+++ Caso1/src/Mensaje.java
@@ -1,4 +1,18 @@
 
 public class Mensaje {
+	
+	private int contenido;
+	
+	public Mensaje(int cont) {
+		this.contenido = cont;
+	}
 
+	public int getContenido() {
+		return contenido;
+	}
+
+	public void setContenido(int contenido) {
+		this.contenido = contenido;
+	}
+	
 }
