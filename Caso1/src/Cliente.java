7569cb87b7b0d50adafb733c494c1ffb504b50a5
na.alvarado <na.alvarado@uniandes.edu.co>
2020-09-08 18:27:49 -0500

    parte de cliente

--- Caso1/src/Cliente.java
+++ Caso1/src/Cliente.java
@@ -1,4 +1,22 @@
 
-public class Cliente {
+public class Cliente extends Thread {
+	
+	private Mensaje[] mensajes;
+	private int id;
+	private Buffer buff;
+	
+	public Cliente(int pId, Buffer pBuff, int numMens) {
+		mensajes = new Mensaje[numMens];
+		this.id = pId;
+		this.buff = pBuff;
+		for(int i = 0; i < mensajes.length; i++) {
+			int r = (int)(Math.random() * (100 - 1 +1) + 1);
+			mensajes[i] = new Mensaje(r);
+		}
+	}
+	
+	public void run() {
+		//TODO
+	}
 
 }
