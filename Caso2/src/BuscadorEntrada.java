ab0e0019869900730f80f7605e9d8396041066d2
na.alvarado <na.alvarado@uniandes.edu.co>
2020-11-03 09:50:59 -0500

    Mejora un poco la complegidad pero ya no se pueden entrar hashes por String, se crea clase para hacer un nuevo algoritmo con threads

---
+++ Caso2/src/BuscadorEntrada.java
@@ -0,0 +1,12 @@
+
+public class BuscadorEntrada extends Thread {
+	private int tamano;
+	
+	public BuscadorEntrada(int tam) {
+		this.tamano = tam;
+	}
+	
+	public void run() {
+		
+	}
+}
--- Caso2/src/ManejadorEncriptacion.java
+++ Caso2/src/ManejadorEncriptacion.java
@@ -1,5 +1,6 @@
 import java.security.MessageDigest;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ManejadorEncriptacion {
 		
@@ -10,61 +11,54 @@ public class ManejadorEncriptacion {
 		return hash.digest();
 	}	
 	
-	public static String identificar_entrada(String hash, String algo) throws Exception {
+	public static String identificar_entrada(byte[] hash, String algo) throws Exception {
 		for(int a = 97; a < 123; a++) {
 			char c1 = (char)a;
 			String sc1 = c1+"";
 			byte[] codigo1 = generar_codigo(sc1, algo);
-			String sh1 = imprimirHash(codigo1);
-			if(sh1.equals(hash)) {
+			if(Arrays.equals(hash, codigo1)) {
 				return sc1;
 			}
 			for(int b = 97; b < 123; b++) {
 				char c2 = (char)b;
 				String sc2 = c1+""+c2+"";
 				byte[] codigo2 = generar_codigo(sc2, algo);
-				String sh2 = imprimirHash(codigo2);
-				if(sh2.equals(hash)) {
+				if(Arrays.equals(hash, codigo2)) {
 					return sc2;
 				}
 				for(int c = 97; c < 123; c++) {
 					char c3 = (char)c;
 					String sc3 = sc2+c3+"";
 					byte[] codigo3 = generar_codigo(sc3, algo);
-					String sh3 = imprimirHash(codigo3);
-					if(sh3.equals(hash)) {
+					if(Arrays.equals(hash, codigo3)) {
 						return sc3;
 					}
 					for(int d = 97; d < 123; d++) {
 						char c4 = (char)d;
 						String sc4  = sc3+c4+"";
 						byte[] codigo4 = generar_codigo(sc4, algo);
-						String sh4 = imprimirHash(codigo4);
-						if(sh4.equals(hash)) {
+						if(Arrays.equals(hash, codigo4)) {
 							return sc4;
 						}
 						for(int e = 97; e < 123; e++) {
 							char c5 = (char)e;
 							String sc5 = sc4+c5+"";
 							byte[] codigo5 = generar_codigo(sc5, algo);
-							String sh5 = imprimirHash(codigo5);
-							if(sh5.equals(hash)) {
+							if(Arrays.equals(hash, codigo5)) {
 								return sc5;
 							}
 							for(int f = 97; f < 123; f++) {
 								char c6 = (char)f;
 								String sc6 = sc5+c6+"";
 								byte[] codigo6 = generar_codigo(sc6, algo);
-								String sh6 = imprimirHash(codigo6);
-								if(sh6.equals(hash)) {
+								if(Arrays.equals(hash, codigo6)) {
 									return sc6;
 								}
 								for(int g = 97; g < 123; g++) {
 									char c7 = (char)g;
 									String sc7 = sc6+c7+"";
 									byte[] codigo7 = generar_codigo(sc7, algo);
-									String sh7 = imprimirHash(codigo7);
-									if(sh7.equals(hash)) {
+									if(Arrays.equals(hash, codigo7)) {
 										return sc7;
 									}
 								}
@@ -90,15 +84,16 @@ public class ManejadorEncriptacion {
 	
 	public static void main(String args[]) {
 		// NO PONGA VALORES MAYORES A 'a' EN EL PRIMER CARACTER
-		// NO PONGA VALORES MOTORES A 'b' EN EL SEGUNDO CARACTER
-		String t = "abccccc";
+		// NO PONGA VALORES MOTORES A 'f' EN EL SEGUNDO CARACTER
+		String t = "afbcccc";
 		String a = "MD5";
 		try {
 			byte[] h = generar_codigo(t,a);
 			String hash = imprimirHash(h);
 			System.out.println(hash);
+			
 			long stIE = System.currentTimeMillis();
-			System.out.println(identificar_entrada(hash,a));
+			System.out.println(identificar_entrada(h,a));
 			long finIE = System.currentTimeMillis();
 			long timeIE = (finIE - stIE)/1000;
 			System.out.println("Se demoro "+timeIE+"s en encontrar la entrada!");
